6b29bdf654e2e2ce3468636a8b7050f606fd9df5
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-11 00:43:58 -0500

    renamed js to javascripts

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var session = require('express-session');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+var mongoose = require('mongoose');
 
 var app = express();
+
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+mongoose.connect(mongoURI)
 
 // Set up Templating
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
@@ -15,11 +20,27 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: true
+}));
 
 // single route
 app.get('/', function(req, res) {
-	res.render('home');
+	console.dir(req.cookies);
+		console.dir(req.session);
+		var message;
+		if (req.session.counter) {
+			req.session.counter++;
+			message = "Hello again! Thanks for visiting " + req.session.counter + " times";
+		} else {
+			message = "Hello, thanks for visiting this site!";
+			req.session.counter = 1;
+		}
+		res.send(message);
 })
 
+var PORT = process.env.PORT || 3000;
 app.listen(PORT);
 
