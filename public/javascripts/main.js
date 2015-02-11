e69dc2167d604f04326c25427a3baf64da81d406
themythicaldrago <ryan.louie@students.olin.edu>
2015-02-11 13:35:16 -0500

    refactored login form jQuery + DOM

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,23 @@
+
+function onError() {
+	console.error('Somthing went wrong!')
+}
+
 $loginForm = $('form#login')
 $loginForm.submit( function(e) {
 	e.preventDefault();
-	name = $(this).children('input').val()
-	$.post('/login', {'name': name}, function(data) {
-		alert('Welcome ' + data.name);
+
+	// Get some values from elements on the page:
+	var $form = $( this )
+	var name = $form.find( "input[name='username']" ).val()
+    var url = $form.attr( "action" );
+
+	login = $.post('/login', {'name': name})
+		
+	login.done(function(data) {
+		alert('Welcome ' + data.name)
 	});
+
+	login.error(onError)
+
 });
